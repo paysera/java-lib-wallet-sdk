2707505cc685e0cb6ed61063268b22936ebcac22
luksiv <l.sivickas@gmail.com>
2020-07-28 12:33:34 +0300

    Change ConfirmationTranslationParameters transferSums

--- library/src/main/java/com/paysera/sdk/wallet/entities/confirmations/ConfirmationTranslationParameters.java
+++ library/src/main/java/com/paysera/sdk/wallet/entities/confirmations/ConfirmationTranslationParameters.java
@@ -1,10 +1,9 @@
 package com.paysera.sdk.wallet.entities.confirmations;
 
 import com.google.gson.annotations.SerializedName;
-import org.joda.money.Money;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class ConfirmationTranslationParameters {
 
@@ -12,14 +11,21 @@ public class ConfirmationTranslationParameters {
     private String phone;
     @SerializedName("%email%")
     private String email;
+
+    public List<Map<String, String>> getTransfersMoneySums() {
+        return transfersMoneySums;
+    }
+
+    public void setTransfersMoneySums(List<Map<String, String>> transfersMoneySums) {
+        this.transfersMoneySums = transfersMoneySums;
+    }
+
     @SerializedName("transfers_money_sums")
-    private List<Money> transfersMoneySums = new ArrayList<>();
+    private List<Map<String, String>> transfersMoneySums;
     @SerializedName("beneficiary_account")
     private String beneficiaryAccount;
     @SerializedName("transfers_count")
     private Integer transfersCount;
-
-
 
     public String getPhone() {
         return phone;
@@ -35,14 +41,6 @@ public class ConfirmationTranslationParameters {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public List<Money> getTransfersMoneySums() {
-        return transfersMoneySums;
-    }
-
-    public void setTransfersMoneySums(List<Money> transfersMoneySums) {
-        this.transfersMoneySums = transfersMoneySums;
     }
 
     public String getBeneficiaryAccount() {
