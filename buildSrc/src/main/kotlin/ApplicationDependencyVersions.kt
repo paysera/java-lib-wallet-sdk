09860c35b1e201a3b69eea3bc55541cfbc1bd385
Gilbert Parreno <gilbertparreno95@gmail.com>
2021-06-03 15:35:51 +0800

    fix versions

--- buildSrc/src/main/kotlin/ApplicationDependencyVersions.kt
+++ buildSrc/src/main/kotlin/ApplicationDependencyVersions.kt
@@ -1,10 +1,14 @@
 object ApplicationDependencyVersions {
     const val kotlin = "1.4.32"
     const val kotlinCoroutinesCore = "1.3.9"
-    const val retrofit = "2.9.0"
-    const val loggingInterceptor = "4.7.2"
     const val gson = "2.6.2"
     const val jodaMoney = "0.12"
     const val certificateTransparency ="0.3.0"
+    const val json = "20160212"
+    const val bolts = "1.4.0"
+    const val retrofit = "2.0.2"
+    const val okhttp3 = "3.10.0"
+    const val loggingInterceptor = "3.10.0"
+    const val apacheCommonsSdk = "1.10"
     const val junit = "4.12"
 }
