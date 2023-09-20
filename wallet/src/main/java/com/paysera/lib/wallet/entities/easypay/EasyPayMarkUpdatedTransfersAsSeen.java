6387d95c0fb3f0a9771cbf8433b8d20ffa755e40
Vitali Tsikhanovich <vitali.tsikhanovich@paysera.net>
2023-09-20 11:06:51 +0300

    remove redundant annotation

--- wallet/src/main/java/com/paysera/lib/wallet/entities/easypay/EasyPayMarkUpdatedTransfersAsSeen.java
+++ wallet/src/main/java/com/paysera/lib/wallet/entities/easypay/EasyPayMarkUpdatedTransfersAsSeen.java
@@ -1,10 +1,8 @@
 package com.paysera.lib.wallet.entities.easypay;
 
-import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 public class EasyPayMarkUpdatedTransfersAsSeen {
-    @SerializedName("epay_transaction_ids")
     private List<Long> epayTransactionIds;
 
     public List<Long> getEpayTransactionIds() {
